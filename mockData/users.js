b954ff08407d604ecbe34451d1162e6a2ac41ba1
Aliaksandr <aliaksandr_talkach@epam.com>
2020-05-15 16:21:35 +0300

    Update mock data

--- mockData/users.js
+++ mockData/users.js
@@ -30,14 +30,3 @@ exports.getPosts = () => {
     }
   ];
 }
-
-const comments = {
-  "asd": [
-    {
-      "text": "What is funny day",
-      "user": "Max"
-    }
-  ]
-}
-
-export default comments; 
